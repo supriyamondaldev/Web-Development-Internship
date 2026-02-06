028ab1f9cdb3c3b9379728ca9f1f826623f88561
Supriya Mondal <141431019+supriyamondaldev@users.noreply.github.com>
2026-02-07 00:22:06 +0530

    Delete Responsive landing page directory

--- Responsive landing page/logic.js
+++
@@ -1,9 +0,0 @@
-const menuToggle = document.getElementById('mobile-menu');
-const nav = document.querySelector('nav ul');
-
-menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('active');
-});
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    document.querySelector('header nav').classList.toggle('active');
-});
